5a108c6d728643dc7a18290090756118d14fdcd6
Satoshi Ota <sota.developer@gmail.com>
2020-09-07 13:43:18 +0900

    Add endpoint get

--- handlers/user.go
+++ handlers/user.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"context"
 	"net/http"
+	"strconv"
 
 	"github.com/dev-sota/sqlboiler-example/models"
 	"github.com/labstack/echo/v4"
@@ -14,4 +15,10 @@ func CreateUser(c echo.Context) error {
 	u := &models.User{Name: name}
 	_ = u.Insert(context.Background(), db, boil.Infer())
 	return c.JSON(http.StatusCreated, u)
+}
+
+func GetUser(c echo.Context) error {
+	id, _ := strconv.ParseUint(c.Param("id"), 10, 64)
+	u, _ := models.FindUser(context.Background(), db, id)
+	return c.JSON(http.StatusOK, u)
 }
